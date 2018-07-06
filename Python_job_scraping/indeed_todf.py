d28ec917faa6b16c20062a8f1410412b0f86186c
Murray <clayton.murray@citizant.com>
2018-07-06 14:46:43 -0400

    forgot file

--- Python_job_scraping/indeed_todf.py
+++ Python_job_scraping/indeed_todf.py
@@ -1,31 +1,32 @@
-import pandas as pd
+# import pandas as pd
 from bs4 import BeautifulSoup
 
-def doconvert(result):#, dataframe):
-	indeed_dict = {}
+def doconvert(result):
+	# indeed_dict = {}
 	
 	# Title
 	title_tag = result.h2.a.get('title')
 
 	# Company
-	comp_tag = result.span.a.contents
+	# comp_tag = result.span.a.contents
 
 	# Location
-	loc_tag = result.find(class_ = 'location').contents
+	loc_tag = result.find(class_ = 'location').contents[0]
 
 	# Summary
-	sum_tag = result.tr.td.div.find(class_ = 'summary').contents
+	# sum_tag = result.tr.td.div.find(class_ = 'summary').contents
 
 	# Experiencelist
-	exp_tag = result.tr.td.div.find(class_ = 'experienceList').contents
+	# exp_tag = result.tr.td.div.find(class_ = 'experienceList').contents
 
 	# Listing age
-	age_tag = result.tr.td.find(class_ = 'date').contents
+	# age_tag = result.tr.td.find(class_ = 'date').contents
 
 	# job link
-	link_tag = 'www.indeed.com' + result.h2.a.get('href')
+	# link_tag = 'www.indeed.com' + result.h2.a.get('href')
 
 	# Populate dictionary
-	indeed_dict[title_tag] = [comp_tag, loc_tag, sum_tag, exp_tag, age_tag, link_tag]
+	# indeed_dict[title_tag] = [comp_tag, loc_tag, sum_tag, exp_tag, age_tag, link_tag]
 
-	return indeed_dict
+	return title_tag, loc_tag
+	# return indeed_dict
---
+++ Python_job_scraping/test_result.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import indeed_todf as todf
+
+with open('scrape_output.txt') as file:
+	soup = BeautifulSoup(file, 'html.parser')
+
+
+no_ad_list = [div for div in soup.find_all('div') if div.get('data-tn-component') == 'organicJob']
+
+# print(no_ad_list)
+# print(len(no_ad_list))
+
+def exc(result):
+# 	return result.h2.a.get('title')
+	return result.span.a
+	# return type(result)
+
+# print(list(map(exc, no_ad_list)))
+# print(list(map(todf.doconvert, no_ad_list)))
+
+print(no_ad_list[0].span.a.contents)
