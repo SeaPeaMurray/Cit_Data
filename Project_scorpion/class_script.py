b2ab11b01660e2cd81721ed0ea6fdc22e778749f
Clayton Murray <claypaulmurray@gmail.com>
2019-01-18 10:29:12 -0500

    Working on class file for script.

--- Project_scorpion/class_script.py
+++ Project_scorpion/class_script.py
@@ -12,18 +12,23 @@ class usaspendingobj:
 		self.original = df
 
 	def join_vehicle_work(self, vehicle, work):
+		# print(self.original)
 		self.vehicle = pd.read_csv(vehicle, names=['parent_award_id', 'contract_vehicle'])
 		self.work = pd.read_csv(work)
 		self.work.columns = ['parent_award_id', 'award_id_piid', 'work']
-		join_vehicle = pd.merge(self.original, self.vehicle, on='parent_award_id', how='left')
-		self.join_vw = pd.merge(join_vehicle, self.work, on=['parent_award_id', 'award_id_piid'], how='left')
+		self.join_vehicle = pd.merge(self.original, self.vehicle, on='parent_award_id', how='left')
+		self.join_vw = pd.merge(self.join_vehicle, self.work, on=['parent_award_id', 'award_id_piid'], how='left')
 
 	def namespace(self):
 		self.test = self.original.head()
 
 myobj = usaspendingobj('_Data/TREAS/api_bulk_listmonthlyfiles/2015_020_Contracts_Full_20181212_1.csv')
 
-print(myobj.original.head(1))
-joined = myobj.join_vehicle_work('_Data/TREAS/ContractVehicles.csv', '_Data/TREAS/Work.csv')
+
+myobj.join_vehicle_work('_Data/TREAS/ContractVehicles.csv', '_Data/TREAS/Work.csv')
+
+# print(myobj.original.head(1))
 # print(joined.join_vw.head())
-print(type(joined.vehicle))
+# print(type(joined.vehicle))
+# print(dir(myobj))
+print(myobj.join_vw)
